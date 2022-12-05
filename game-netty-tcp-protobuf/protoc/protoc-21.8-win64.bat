978d395c0215066e89c9e0c6b19ac442d56ee773
caoguiming <1677664380@qq.com>
2022-12-05 14:14:07 +0800

    【i】【CmcOrg】

--- game-netty-tcp-protobuf/protoc/protoc-21.8-win64.bat
+++ game-netty-tcp-protobuf/protoc/protoc-21.8-win64.bat
@@ -2,6 +2,6 @@
 chcp 65001
 for %%i in (../proto/*.proto) do (
     echo 编译协议：%%i
-    protoc-21.8-win64.exe --proto_path=../proto --java_out=../src/main/java %%i
+    protoc-21.9-win64.exe --proto_path=../proto --java_out=../src/main/java %%i
 )
 pause
