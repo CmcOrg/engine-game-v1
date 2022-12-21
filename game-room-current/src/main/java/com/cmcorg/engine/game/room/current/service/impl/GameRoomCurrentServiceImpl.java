21a3bb3291f77f5bb23c1b69977fd64c0065c77c
caoguiming <1677664380@qq.com>
2022-12-21 18:41:35 +0800

    【i】【CmcOrg】

--- game-room-current/src/main/java/com/cmcorg/engine/game/room/current/service/impl/GameRoomCurrentServiceImpl.java
+++ game-room-current/src/main/java/com/cmcorg/engine/game/room/current/service/impl/GameRoomCurrentServiceImpl.java
@@ -128,7 +128,7 @@ public class GameRoomCurrentServiceImpl extends ServiceImpl<GameRoomCurrentMappe
             // 拿到：返回值
             return getGameRoomCurrentJoinRoomVO(currentUserId, gameSocketServerDO);
 
-        })
+        });
 
     }
 
