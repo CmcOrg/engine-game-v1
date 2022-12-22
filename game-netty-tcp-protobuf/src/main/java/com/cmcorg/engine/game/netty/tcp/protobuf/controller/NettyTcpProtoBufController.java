35e10218c79f6ebbcac7f361ef8651474cfb9c42
caoguiming <1677664380@qq.com>
2022-12-22 15:34:48 +0800

    【i】【CmcOrg】

--- game-netty-tcp-protobuf/src/main/java/com/cmcorg/engine/game/netty/tcp/protobuf/controller/NettyTcpProtoBufController.java
+++ game-netty-tcp-protobuf/src/main/java/com/cmcorg/engine/game/netty/tcp/protobuf/controller/NettyTcpProtoBufController.java
@@ -1,7 +1,6 @@
 package com.cmcorg.engine.game.netty.tcp.protobuf.controller;
 
 import com.cmcorg.engine.game.netty.tcp.protobuf.model.vo.NettyTcpProtoBufVO;
-import com.cmcorg.engine.web.auth.exception.BaseBizCodeEnum;
 import com.cmcorg.engine.web.netty.boot.annotation.NettyController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +9,7 @@ public class NettyTcpProtoBufController {
 
     @RequestMapping(value = "/ping")
     public NettyTcpProtoBufVO ping() {
-        return NettyTcpProtoBufVO.ok(BaseBizCodeEnum.OK);
+        return NettyTcpProtoBufVO.ok("pong");
     }
 
 }
