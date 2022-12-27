0a42106cf9c785c1189421e3d2636dc61029a88b
法外狂徒李四 <dimensional_logic@qq.com>
2022-12-27 22:45:17 +0800

    【i】【CmcOrg】

--- game-netty-tcp-protobuf/src/main/java/com/cmcorg/engine/game/netty/tcp/protobuf/model/bo/BasePlayerBO.java
+++ game-netty-tcp-protobuf/src/main/java/com/cmcorg/engine/game/netty/tcp/protobuf/model/bo/BasePlayerBO.java
@@ -5,8 +5,6 @@ import com.cmcorg.engine.game.room.current.model.entity.GameRoomCurrentDO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import java.util.Objects;
-
 @Data
 public class BasePlayerBO {
 
@@ -21,22 +19,5 @@ public class BasePlayerBO {
 
     @Schema(description = "用户当前房间的 配置信息")
     private GameRoomConfigDO gameRoomConfigDO;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        BasePlayerBO that = (BasePlayerBO)o;
-        return getGameUserId().equals(that.getGameUserId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getGameUserId());
-    }
 
 }
